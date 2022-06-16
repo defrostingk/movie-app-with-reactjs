8c96b15aecdeb855cb6cc1dbd433901079f1abd4
Haedong Kim <haedong1111@gmail.com>
2022-06-16 17:12:01 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onChange = (event) => setKeyword(event.target.value);
   const onClick = () => setCounter((current) => current + 1);
-  console.log('I run all the time');
   useEffect(() => {
     console.log('Call the API...');
   }, []);
+  useEffect(() => {
+    if (keyword.length > 5) console.log('Search for', keyword);
+  }, [keyword]);
+  useEffect(() => {
+    console.log('Counter has been changed');
+  }, [counter]);
+  useEffect(() => {
+    console.log('Keyword or counter has been changed');
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Search here...'
+      ></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
