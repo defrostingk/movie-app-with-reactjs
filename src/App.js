02e44f25edc19c2bf7d43df21271b0d142986b22
Haedong Kim <haedong1111@gmail.com>
2022-06-17 15:59:34 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setCounter((current) => current + 1);
+function Hello() {
   useEffect(() => {
-    console.log('Call the API...');
+    console.log('created :)');
+    return () => console.log('destroyed :(');
   }, []);
-  useEffect(() => {
-    if (keyword.length > 5) console.log('Search for', keyword);
-  }, [keyword]);
-  useEffect(() => {
-    console.log('Counter has been changed');
-  }, [counter]);
-  useEffect(() => {
-    console.log('Keyword or counter has been changed');
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type='text'
-        placeholder='Search here...'
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
