9bde0ab6614e413d7b56da091eccce4fd1f6d025
Haedong Kim <haedong1111@gmail.com>
2022-06-19 01:02:55 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo('');
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [price, setPrice] = useState('none');
+  const [balance, setBalance] = useState(0);
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const onChangeSelect = (event) => {
+    setPrice(event.target.value);
+  };
+  const onChangeBalance = (event) => {
+    setBalance(event.target.value);
   };
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          text='text'
-          placeholder='Write your to do...'
-        />
-        <button>Add To Do</button>
-      </form>
+      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onChangeSelect}>
+          <option key='-1' value='none'>
+            Select a coin...
+          </option>
+          {coins.map((coin, idx) => (
+            <option key={idx} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+
       <hr />
-      <ul>
-        {toDos.map((toDo, idx) => (
-          <li key={idx}>{toDo}</li>
-        ))}
-      </ul>
+      <span>If you have $</span>
+      <input value={balance} onChange={onChangeBalance} type='number' />
+      <span>, you can get {price === 'none' ? 0 : balance / price} coins</span>
     </div>
   );
 }
